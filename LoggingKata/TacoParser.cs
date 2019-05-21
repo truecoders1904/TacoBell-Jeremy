2d16c86ac72f61dde76d0d2ab02af4bc2eb5a8ed
Jeremy <jlong.223.jl@gmail.com>
2019-05-21 15:20:57 -0500

    added changes and steps to get the distance between two taco bells in alabama

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -7,12 +7,67 @@
     {
         readonly ILog logger = new TacoLogger();
         
+        
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
 
-            // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            if (line == null)
+            {
+                return null;
+
+            }
+               
+            var cells = line.Split(',');
+
+             if (cells.Length < 3)
+            {
+                //Log that and return null
+                logger.LogWarning("Not enough data to parse");
+                return null;
+            }
+
+
+            double latitude;
+            double longitude;
+
+            try
+            {
+
+                latitude = double.Parse(cells[0]);
+                 longitude = double.Parse( cells[1]);
+            }
+
+
+                 
+            catch
+            {
+                return null;
+                   
+            }
+
+            string name = cells[2];
+
+            if(name.Length == 0)
+            {
+                return null;
+            }
+
+            TacoBell tacoBell = new TacoBell();
+            tacoBell.Name = name;
+
+            Point location = new Point();
+            location.Latitude = latitude;
+            location.Longitude = longitude;
+
+            tacoBell.Location = location;
+
+            return tacoBell;
+                
+
+                
+
+          
         }
     }
 }
